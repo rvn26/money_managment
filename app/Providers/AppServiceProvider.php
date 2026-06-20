1df966fe25d09c87b73f137632b0cde45c2c84e7
rvn26 <bigboos2604@gmail.com>
2026-06-20 15:59:42 +0700

    fix firebase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,12 +24,26 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
-        
+
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
         if (request()->header('x-forwarded-proto') === 'https') {
             URL::forceScheme('https');
+        }
+
+        $targetPath = storage_path('app/firebase/service-account.json');
+
+        // Jika file belum ada di server, dan string Base64 tersedia di .env
+        if (!file_exists($targetPath) && env('FIREBASE_CREDENTIALS_BASE64')) {
+            // Buat foldernya jika belum ada
+            if (!is_dir(dirname($targetPath))) {
+                mkdir(dirname($targetPath), 0755, true);
+            }
+
+            // Decode isi Base64 dan tulis menjadi file .json fisik kembali
+            $jsonContent = base64_decode(env('FIREBASE_CREDENTIALS_BASE64'));
+            file_put_contents($targetPath, $jsonContent);
         }
     }
 }
